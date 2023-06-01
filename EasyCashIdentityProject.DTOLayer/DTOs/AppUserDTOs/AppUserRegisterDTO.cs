21e96308ae2188fab24a437d5a99f7ee9f180a32
Çağrı Keleş <cagri.keless@hotmail.com>
2023-06-01 23:09:41 +0300

    Adding Interfaces and +

---
+++ EasyCashIdentityProject.DTOLayer/DTOs/AppUserDTOs/AppUserRegisterDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DTOLayer.DTOs.AppUserDTOs
+{
+    public class AppUserRegisterDTO
+    {
+        //[Required(ErrorMessage="Ad alanı zorunludur")]
+        //[Display(Name="İsim:")]
+        //[MaxLength(30,ErrorMessage ="En Fazla 30 karakter girebilirsiniz.")]
+        
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
